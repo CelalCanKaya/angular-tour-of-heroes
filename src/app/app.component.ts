3213cd35020432db78e9c03c687dc211c36b8c8a
Celal Can Kaya <cckaya08@gmail.com>
2021-06-07 03:27:09 +0300

    Header changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-root',
-  template: `
-    <h1>{{title}}</h1>
-    <div class="header-bar"></div>
-    <nav>
-      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-      <a routerLink="/pokemons" routerLinkActive="active">Pokemons</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `,
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Tour of Pokemons';
